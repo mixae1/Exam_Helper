652f5cce2889c339bdb0b959e8db6ce64d410c38
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2020-10-02 21:10:57 +0300

    присоединили в бд , пока с шаблонами микромягких , шаблоны нужны свои

---
+++ Exam_Helper/Models/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Exam_Helper
+{
+    public partial class Question
+    {
+        public Question()
+        {
+            ATest = new HashSet<ATest>();
+        }
+
+        public int Id { get; set; }
+        public string Definition { get; set; }
+        public string Title { get; set; }
+        public string Proof { get; set; }
+        public string Author { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string TagIds { get; set; }
+
+        public virtual ICollection<ATest> ATest { get; set; }
+    }
+}
