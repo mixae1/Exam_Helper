c2b6042aa672f2cb716578e684582b8a699e86d9
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2020-10-17 20:54:58 +0300

    сделали выбор вопросов при создании паков

---
+++ Exam_Helper/ViewsModel/QuestionsListForPackCreatingModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam_Helper.ViewsModel
+{   
+
+    public class QuestionsListForPackCreatingModel
+    {
+      public  QuestionForPackCreatingModel[] Questions { get; set; }
+    }
+
+    public class QuestionForPackCreatingModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+       public bool IsSelected { get; set; }
+    }
+
+}
