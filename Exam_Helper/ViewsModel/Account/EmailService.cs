51b6829b2577ff6e0b4c857d78db0467f13ca2d8
Anoth <mixaetel@gmail.com>
2021-02-19 22:47:08 +0300

    добавил все необходимые файлы для восстановления пароля, осталось привязать почту

---
+++ Exam_Helper/ViewsModel/Account/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+using MailKit;
+namespace Exam_Helper.Services
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "ex.helper@yandex.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient(new ProtocolLogger("smtp.log")))
+            {
+                await client.ConnectAsync("smtp.gmail.com", 465, true);
+                await client.AuthenticateAsync("ex.helper@yandex.ru", "70aed950fe8f6d4f096d08edaf28051f79f8beaa");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
