17da63949e8ffb77d24cc59acac8086bfa2a9946
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2021-02-27 15:11:47 +0300

    first version user settings of multitesting

--- Exam_Helper/ViewsModel/Tests/TestChoiceViewModel.cs
+++ Exam_Helper/ViewsModel/Tests/TestChoiceViewModel.cs
@@ -17,6 +17,8 @@ namespace Exam_Helper.ViewsModel
 
         public string ServiceInfo { get; set; }
 
+        public string MultiTestingInfoTests { get; set; }
+
         [Required(ErrorMessage = "Выберите один из вариантов")]
         [Range(1,100000,ErrorMessage ="Выберите один из вариантов")]
         public int SelectedId { get; set; }
