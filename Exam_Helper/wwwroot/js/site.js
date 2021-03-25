e7346e1210d0720754a1163928b2bc9439e6d82a
EfimKornienko <efim3009@gmail.com>
2021-03-25 23:03:07 +0300

    UI

--- Exam_Helper/wwwroot/js/site.js
+++ Exam_Helper/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('.change-color').click(function () {
+    // $(".card").addClass("border-info")
+    let color = $(this).css("background-color").toString();
+    console.log(color);
+    $(".card").css("border-color",color)
+    let bc = $(".card").css("border-color")
+    console.log(bc);
+});
