b18ef99cf9d04b4e780400968d7dd594ed90106c
EfimKornienko <efim3009@gmail.com>
2021-03-26 00:55:35 +0300

    UI + LocalStorage + Fix

--- Exam_Helper/wwwroot/js/site.js
+++ Exam_Helper/wwwroot/js/site.js
@@ -3,11 +3,14 @@
 
 // Write your JavaScript code.
 
+$(".card").css("border-color",localStorage.getItem("bc"));
 $('.change-color').click(function () {
     // $(".card").addClass("border-info")
     let color = $(this).css("background-color").toString();
     console.log(color);
     $(".card").css("border-color",color)
+    // $(".btn-outline-info").css("border-color",color)
     let bc = $(".card").css("border-color")
     console.log(bc);
+    localStorage.setItem("bc",bc);
 });
