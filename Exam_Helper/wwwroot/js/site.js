c56444ad6bf8769876d5b742949b84e4409fd4f7
EfimKornienko <efim3009@gmail.com>
2021-05-09 04:44:37 +0300

    Card & Question

--- Exam_Helper/wwwroot/js/site.js
+++ Exam_Helper/wwwroot/js/site.js
@@ -26,10 +26,34 @@ $('.change-color').click(function () {
     $(".draggable-elems").css("border-color",color)
     $(".wrong-text").css("border-color",color)
     $(".miss-words").css("border-color",color)
-    
     $(".test-card").css("border-color",color)
     // $(".btn-outline-info").css("border-color",color)
     let bc = $(".card").css("border-color")
     console.log(bc);
     localStorage.setItem("bc",bc);
 });
+
+$(".check-height").click(function (event){
+    setTimeout(
+        function (){
+            if(Number($(event.target).parents(".card").find(".card-body").css("height").slice(0, length - 2)) < 90){
+                $(event.target).parents(".card").find(".dropdown-menu").css({"height": "75px", "overflow-y": "scroll" });
+            }
+        }, 
+        10);
+})
+
+$(".check-height-user").click(function (event){
+    setTimeout(
+        function (){
+            if(Number($(event.target).parents(".card").find(".card-body").css("height").slice(0, length - 2)) < 90){
+                $(event.target).parents(".card").find(".dropdown-menu").css({"height": "75px", "overflow-y": "scroll"});
+            }
+            else if(Number($(event.target).parents(".card").find(".card-body").css("height").slice(0, length - 2)) > 130){
+                $(event.target).parents(".card").find(".ddm-btn").css("max-height","150px");
+                // $(event.target).parents(".card").find(".dropdown-menu").css("height","145px");
+            }
+        },
+        10);
+})
+
